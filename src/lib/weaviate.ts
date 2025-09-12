ea0c18758e44f41896b7018221ad5969fde9d76e
elevinas123 <elvinas.monstvilas@gmail.com>
2025-09-12 23:07:39 +0300

    weaveate setup

--- src/lib/weaviate.ts
+++ src/lib/weaviate.ts
@@ -1,10 +1,13 @@
 import weaviate, { WeaviateClient } from "weaviate-ts-client";
-import { WEAVIATE_API_KEY, WEAVIATE_URL } from '../config/enviroment';
 
 export const weaviateClient: WeaviateClient = weaviate.client({
-  scheme: "https",
-  host: WEAVIATE_URL!, 
-  apiKey: WEAVIATE_API_KEY
-    ? new weaviate.ApiKey(WEAVIATE_API_KEY)
-    : undefined,
+   scheme: "https",
+   host: process.env.WEAVIATE_URL!,
+   apiKey: process.env.WEAVIATE_API_KEY
+      ? new weaviate.ApiKey(process.env.WEAVIATE_API_KEY)
+      : undefined,
+   headers: {
+      // REQUIRED for text2vec-openai
+      "X-OpenAI-Api-Key": process.env.OPENAI_API_KEY!,
+   },
 });
